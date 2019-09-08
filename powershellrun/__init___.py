892b5fcc29b322986304f922c84d8798b31409f6
Gokul <gokulyc@gmail.com>
2019-09-08 19:06:18 +0530

    powershell commands running

---
+++ powershellrun/__init___.py
@@ -0,0 +1,2 @@
+import runcmdps #no qa
+
---
+++ powershellrun/runcmdps.py
@@ -0,0 +1,16 @@
+from subprocess import Popen,PIPE
+import os
+
+from pathlib import Path, PureWindowsPath
+
+shellpath = r"c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
+# print(PureWindowsPath(shellpath))
+# subprocess.run()
+print("Enter command:")
+ar=input()
+
+p = Popen([shellpath,ar],stdin=PIPE,stdout=PIPE, stderr=PIPE,universal_newlines=True)
+# [ 'ls -l']
+# stdout, stderr =p.communicate()
+print(p.communicate())
+p.kill()

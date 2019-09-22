9c1ec0b862a67efbacea30bb13db3f75c0e36ee3
Gokul <gokulyc@gmail.com>
2019-09-22 12:50:12 +0530

    modified:   DAL/sqlite_ex.py 	modified:   powershellrun/runcmdps.py 	fixed popen

--- DAL/sqlite_ex.py
+++ DAL/sqlite_ex.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine, MetaData, Table, inspect, Column, Integer,
 dbPath = 'Dal/DB.db'
 db_uri = 'sqlite:///{}'.format(dbPath)
 engine = create_engine(db_uri)
+
+# census = db.Table('census', metadata, autoload=True, autoload_with=engine)
 # Create MetaData instance
-# metadata = MetaData(engine, reflect=True)
+connection = engine.connect()
+metadata = MetaData()
+tblCommands = Table('Commands', metadata, autoload=True, autoload_with=engine)
+# print(repr(metadata.tables['Commands']))
 # print(metadata.tables)
 
 
@@ -28,40 +33,42 @@ engine = create_engine(db_uri)
 # table = Table('Commands',metadata,autoload=True)
 
 def InsertCmd(Name, Ctext):
-   """
-   Requires Name of command and Command Text
-   """
-   query = '''Insert into "Commands" (Name,Ctext) Values('{}','{}')'''.format(
-      Name, Ctext)
-   #  print(query)
-   engine.execute(query)
+    """
+    Requires Name of command and Command Text
+    """
+    query = '''Insert into "Commands" (Name,Ctext) Values('{}','{}')'''.format(
+        Name, Ctext)
+    #  print(query)
+    engine.execute(query)
 
 # InsertCmd('ipconfig','ipconfig')
 
 
 # select *
 def get_cmd(cid=0):
-   """
-   Optional Cid value in int format to get the record, use 0 for all records in list type
-   ex :  for _r in get_cmd(0):
-            print(_r)
-   """
-   # print("Cid :",cid)
+    """
+    Optional Cid value in int format to get the record, use 0 for all records in list type
+    ex :  for _r in get_cmd(0):
+             print(_r)
+    """
+    # print("Cid :",cid)
 
-   if(cid == 0):
-      result = engine.execute('SELECT * FROM '+'"Commands"')
-   else:
-      result = engine.execute(('SELECT * FROM '+'"Commands" where Cid={}').format(cid))
-   return result
+    if(cid == 0):
+        result = engine.execute('SELECT * FROM '+'"Commands"')
+    else:
+        result = engine.execute(
+            ('SELECT * FROM '+'"Commands" where Cid={}').format(cid))
+    return result
+
 
 def delete(cid):
-   """
-   need command id (cid) to remove the record
-   ex: delete(4)
-   """
-   query='delete from Commands where Cid={}'.format(cid)
-   #  print(query)
-   engine.execute(query)
+    """
+    need command id (cid) to remove the record
+    ex: delete(4)
+    """
+    query = 'delete from Commands where Cid={}'.format(cid)
+    #  print(query)
+    engine.execute(query)
 
 # for _r in get_cmd(0):
 #     print(_r)
--- powershellrun/runcmdps.py
+++ powershellrun/runcmdps.py
@@ -1,4 +1,4 @@
-from subprocess import Popen,PIPE
+from subprocess import Popen, PIPE
 import os
 
 from pathlib import Path, PureWindowsPath
@@ -7,10 +7,11 @@ shellpath = r"c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
 # print(PureWindowsPath(shellpath))
 # subprocess.run()
 print("Enter command:")
-ar=input()
+ar = input()
 
-p = Popen([shellpath,ar],stdin=PIPE,stdout=PIPE, stderr=PIPE,universal_newlines=True)
+p = Popen([shellpath, ar], stdin=PIPE, stdout=PIPE,
+          stderr=PIPE, universal_newlines=True)
 # [ 'ls -l']
-# stdout, stderr =p.communicate()
-print(p.communicate())
+stdout, stderr = p.communicate()
+print(stdout+"  \n Error :"+stderr)
 p.kill()
